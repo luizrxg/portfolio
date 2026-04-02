78380571bfd3fbced328e7a8da6a4d07c7a42feb
Luiz Gomes <luiz.gomes@run2biz.com>
2026-04-02 14:56:45 -0300

    * Added shadcn, components.json and tailwind * Added FluidGlass component (ReactBits)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.scss";
 import {ReactNode} from "react";
+import {cn} from "@/utils/string";
+import FluidGlass from "@/components/fluid-glass/fluid-glass";
 
 const defaultFont = localFont({
   src: "../../public/assets/fonts/satoshi/Satoshi-Variable.ttf",
@@ -27,10 +29,20 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${defaultFont.className} ${displayFont.variable}`}
+      className={cn(defaultFont.className, displayFont.variable)}
     >
       <body>
         {children}
+        <FluidGlass
+          mode="lens"
+          lensProps={{
+            scale: 0.25,
+            ior: 1.15,
+            thickness: 5,
+            chromaticAberration: 0.1,
+            anisotropy: 0.01
+          }}
+        />
       </body>
     </html>
   );
