3da4834dd7eafd62308ff6c8b8c081c55a1844dc
Luiz Gomes <luiz.gomes@run2biz.com>
2026-04-06 20:21:41 -0300

    * Fixed stickers * Added spikes to experience section divider * Minor stacks section adjustments

--- src/sections/stacks/stacks.tsx
+++ src/sections/stacks/stacks.tsx
@@ -8,8 +8,8 @@ import {ScrollTrigger} from "gsap/ScrollTrigger";
 import './styles.scss';
 import TextPressure from "@/components/text-pressure/text-pressure";
 import ClickSpark from "@/components/click-spark/click-spark";
+import SpikesSVG from "@/assets/svgs/spikes.svg";
 import Image from "next/image";
-import WaveSVG from "@/assets/svgs/waves.svg";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -46,39 +46,62 @@ export default function Stacks() {
           end: '+=400',
           scrub: 1,
         },
-        y: -130,
+        y: -90,
       }
     )
+
+    gsap.fromTo(
+      '.spikes',
+      {
+        x: -7000,
+        rotateX: 180,
+      },
+      {
+        scrollTrigger: {
+          scroller: document.body,
+          trigger: '#stacks',
+          start: 'top top',
+          end: '+=10000',
+          scrub: 1,
+        },
+        rotateX: 180,
+        x: 1000,
+      }
+    )
+
   }, { scope: container })
 
   const stickerItems = useMemo(() => {
 
     return [
-      { imageSrc: '/assets/images/react.png',      peelDirection: 41,  rotate: -11, initialPosition: { x: 72,  y: 648 } },
-      { imageSrc: '/assets/images/node.png',       peelDirection: 227, rotate: -3,  initialPosition: { x: 156, y: 566 } },
-      { imageSrc: '/assets/images/angular.png',    peelDirection: 153, rotate: 12,  initialPosition: { x: 854, y: 84 } },
-      { imageSrc: '/assets/images/gsap.png',       peelDirection: 318, rotate: -8,  initialPosition: { x: 493, y: 247 } },
-      { imageSrc: '/assets/images/typescript.png', peelDirection: 19,  rotate: 9,   initialPosition: { x: 932, y: 361 } },
-      { imageSrc: '/assets/images/bun.png',        peelDirection: 204, rotate: 6,   initialPosition: { x: 771, y: 188 } },
-      { imageSrc: '/assets/images/sass.png',       peelDirection: 344, rotate: -13, initialPosition: { x: 664, y: 43 } },
-      { imageSrc: '/assets/images/next.png',       peelDirection: 88,  rotate: 10,  initialPosition: { x: 304, y: 117 } },
-      { imageSrc: '/assets/images/vite.png',       peelDirection: 271, rotate: -9,  initialPosition: { x: 51,  y: 236 } },
-      { imageSrc: '/assets/images/npm.png',        peelDirection: 126, rotate: 3,   initialPosition: { x: 200, y: 300 } },
-      { imageSrc: '/assets/images/yarn.png',       peelDirection: 239, rotate: -2,  initialPosition: { x: 529, y: 400 } },
-      { imageSrc: '/assets/images/java.png',       peelDirection: 12,  rotate: 8,   initialPosition: { x: 427, y: 612 } },
-      { imageSrc: '/assets/images/docker.png',     peelDirection: 329, rotate: -5,  initialPosition: { x: 133, y: 686 } },
-      { imageSrc: '/assets/images/jest.png',       peelDirection: 63,  rotate: 2,   initialPosition: { x: 28,  y: 399 } },
-      { imageSrc: '/assets/images/git.png',        peelDirection: 287, rotate: 14,  initialPosition: { x: 575, y: 194 } },
-      { imageSrc: '/assets/images/tailwind.png',   peelDirection: 173, rotate: -7,  initialPosition: { x: 889, y: 271 } },
-      { imageSrc: '/assets/images/postgresql.png', peelDirection: 301, rotate: 5,   initialPosition: { x: 715, y: 444 } },
-      { imageSrc: '/assets/images/springboot.png', peelDirection: 146, rotate: -4,  initialPosition: { x: 626, y: 557 } },
-      { imageSrc: '/assets/images/c#.png',         peelDirection: 258, rotate: 11,  initialPosition: { x: 246, y: 521 } },
-      { imageSrc: '/assets/images/figma.png',      peelDirection: 35,  rotate: -10, initialPosition: { x: 373, y: 312 } },
-      { imageSrc: '/assets/images/zod.png',        peelDirection: 352, rotate: 7,   initialPosition: { x: 112, y: 95 } },
-      { imageSrc: '/assets/images/tanstack.png',   peelDirection: 97,  rotate: -6,  initialPosition: { x: 472, y: 81 } },
-      { imageSrc: '/assets/images/three.png',      peelDirection: 214, rotate: -15, initialPosition: { x: 400, y: 151 } },
-      { imageSrc: '/assets/images/storybook.png',  peelDirection: 281, rotate: -1,  initialPosition: { x: 847, y: 583 } },
-      { imageSrc: '/assets/images/graphql.png',    peelDirection: 118, rotate: 13,  initialPosition: { x: 289, y: 292 } },
+      { imageSrc: '/assets/images/react.png',      peelDirection: 41,  rotate: -11, initialPosition: { x: 720, y: 60  } },
+      { imageSrc: '/assets/images/node.png',       peelDirection: 227, rotate: -3,  initialPosition: { x: 266, y: 406 } },
+      { imageSrc: '/assets/images/angular.png',    peelDirection: 153, rotate: 12,  initialPosition: { x: 704, y: 84  } },
+      { imageSrc: '/assets/images/gsap.png',       peelDirection: 318, rotate: -8,  initialPosition: { x: 493, y: 207 } },
+      { imageSrc: '/assets/images/typescript.png', peelDirection: 19,  rotate: 9,   initialPosition: { x: 932, y: 321 } },
+      { imageSrc: '/assets/images/bun.png',        peelDirection: 204, rotate: 6,   initialPosition: { x: 720, y: 258 } },
+      { imageSrc: '/assets/images/sass.png',       peelDirection: 344, rotate: -13, initialPosition: { x: 664, y: 0   } },
+      { imageSrc: '/assets/images/next.png',       peelDirection: 88,  rotate: 10,  initialPosition: { x: 304, y: 57  } },
+      { imageSrc: '/assets/images/vite.png',       peelDirection: 271, rotate: -9,  initialPosition: { x: 51,  y: 196 } },
+      { imageSrc: '/assets/images/npm.png',        peelDirection: 126, rotate: 3,   initialPosition: { x: 150, y: 330 } },
+      { imageSrc: '/assets/images/yarn.png',       peelDirection: 239, rotate: -2,  initialPosition: { x: 529, y: 360 } },
+      { imageSrc: '/assets/images/java.png',       peelDirection: 12,  rotate: 8,   initialPosition: { x: 467, y: 482 } },
+      { imageSrc: '/assets/images/docker.png',     peelDirection: 329, rotate: -5,  initialPosition: { x: 103, y: 496 } },
+      { imageSrc: '/assets/images/jest.png',       peelDirection: 63,  rotate: 2,   initialPosition: { x: 28,  y: 359 } },
+      { imageSrc: '/assets/images/git.png',        peelDirection: 287, rotate: 14,  initialPosition: { x: 575, y: 154 } },
+      { imageSrc: '/assets/images/tailwind.png',   peelDirection: 173, rotate: -7,  initialPosition: { x: 789, y: 231 } },
+      { imageSrc: '/assets/images/postgresql.png', peelDirection: 301, rotate: 5,   initialPosition: { x: 645, y: 374 } },
+      { imageSrc: '/assets/images/springboot.png', peelDirection: 146, rotate: -4,  initialPosition: { x: 646, y: 477 } },
+      { imageSrc: '/assets/images/c-sharp.png',    peelDirection: 258, rotate: 11,  initialPosition: { x: 286, y: 481 } },
+      { imageSrc: '/assets/images/figma.png',      peelDirection: 35,  rotate: -10, initialPosition: { x: 373, y: 272 } },
+      { imageSrc: '/assets/images/zod.png',        peelDirection: 352, rotate: 7,   initialPosition: { x: 112, y: 55  } },
+      { imageSrc: '/assets/images/zustand.png',    peelDirection: 97,  rotate: -6,  initialPosition: { x: 472, y: 41  } },
+      { imageSrc: '/assets/images/three.png',      peelDirection: 214, rotate: -15, initialPosition: { x: 390, y: 171 } },
+      { imageSrc: '/assets/images/storybook.png',  peelDirection: 281, rotate: -1,  initialPosition: { x: 847, y: 443 } },
+      { imageSrc: '/assets/images/graphql.png',    peelDirection: 118, rotate: 13,  initialPosition: { x: 289, y: 252 } },
+      { imageSrc: '/assets/images/html.png',       peelDirection: 45,  rotate: 3,   initialPosition: { x: 180, y: 180 } },
+      { imageSrc: '/assets/images/css.png',        peelDirection: 265, rotate: 9,   initialPosition: { x: 400, y: 422 } },
+      { imageSrc: '/assets/images/php.png',        peelDirection: 72, rotate: 0,    initialPosition: { x: 254, y: 10  } },
     ]
   }, []);
 
@@ -101,8 +124,8 @@ export default function Stacks() {
             flex={false}
             fontFamily="var(--font-serif)"
             textColor="#ffffff"
-            minFontSize={300}
-            maxFontSize={300}
+            minFontSize={200}
+            maxFontSize={200}
             lettersId="my-stacks-letter"
           />
         </div>
@@ -123,7 +146,7 @@ export default function Stacks() {
                 initialPosition={sticker.initialPosition}
                 peelDirection={sticker.peelDirection}
                 rotate={sticker.rotate}
-                width={120}
+                width={96}
                 peelBackHoverPct={20}
                 peelBackActivePct={30}
                 shadowIntensity={.5}
@@ -132,6 +155,8 @@ export default function Stacks() {
             ))}
           </div>
         </div>
+        <SpikesSVG className="spikes"/>
+
       </ClickSpark>
     </div>
   )
